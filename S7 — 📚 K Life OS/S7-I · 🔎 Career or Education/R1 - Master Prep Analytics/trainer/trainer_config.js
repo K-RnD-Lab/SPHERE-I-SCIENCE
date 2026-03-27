470a817a0d464a41195dec0d672031407ebeef44
Oksana.Kolisnyk.24y.0 <zazera777@gmail.com>
2026-03-28 01:14:38 +0200

    Update trainer_config.js

--- S7 — 📚 K Life OS/S7-I · 🔎 Career or Education/R1 - Master Prep Analytics/trainer/trainer_config.js
+++ S7 — 📚 K Life OS/S7-I · 🔎 Career or Education/R1 - Master Prep Analytics/trainer/trainer_config.js
@@ -1,3 +1,3 @@
 window.MASTER_PREP_TRAINER_CONFIG = {
-  endpoint: 'https://script.google.com/macros/s/AKfycbyNfCyanV5k5uRNchnMhskx4qw8IYVGXsrPUF6wnByIIWzsuL7HwnnQ9npNUXibgfg-tw/exec'
+  endpoint: 'https://script.google.com/macros/s/AKfycbzDJy4ysMJpXiDXI1_nwZYP1BCx0S1bZ7y9NHmDiD6wPhgfyRb4oXwDRO4twW2NfwNr1A/exec'
 };
