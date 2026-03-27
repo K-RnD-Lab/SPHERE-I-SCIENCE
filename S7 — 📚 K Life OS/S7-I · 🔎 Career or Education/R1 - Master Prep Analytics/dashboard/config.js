3863585ccb19a48c69314c14d061eacefd379445
Oksana.Kolisnyk.24y.0 <zazera777@gmail.com>
2026-03-28 01:12:32 +0200

    Update config.js

--- S7 — 📚 K Life OS/S7-I · 🔎 Career or Education/R1 - Master Prep Analytics/dashboard/config.js
+++ S7 — 📚 K Life OS/S7-I · 🔎 Career or Education/R1 - Master Prep Analytics/dashboard/config.js
@@ -1,10 +1,10 @@
 window.MASTER_PREP_CONFIG = {
-  dataEndpoint: 'https://script.google.com/macros/s/AKfycbyNfCyanV5k5uRNchnMhskx4qw8IYVGXsrPUF6wnByIIWzsuL7HwnnQ9npNUXibgfg-tw/exec',
+  dataEndpoint: 'https://script.google.com/macros/s/AKfycbzDJy4ysMJpXiDXI1_nwZYP1BCx0S1bZ7y9NHmDiD6wPhgfyRb4oXwDRO4twW2NfwNr1A/exec',
   links: {
     repository: 'https://github.com/K-RnD-Lab/SPHERE-I-SCIENCE/tree/main/S7%20%E2%80%94%20%F0%9F%93%9A%20K%20Life%20OS/S7-I%20%C2%B7%20%F0%9F%94%8E%20Career%20or%20Education/R1%20-%20Master Prep Analytics',
     googleSheet: 'https://docs.google.com/spreadsheets/d/1GcgjCJEPDAFtqOwONsfN_np5zdZFe3v2qa2lNzqDZd4/edit?gid=1089917585#gid=1089917585',
     lookerStudio: '',
-    appsScript: 'https://script.google.com/macros/s/AKfycbyNfCyanV5k5uRNchnMhskx4qw8IYVGXsrPUF6wnByIIWzsuL7HwnnQ9npNUXibgfg-tw/exec',
+    appsScript: 'https://script.google.com/macros/s/AKfycbzDJy4ysMJpXiDXI1_nwZYP1BCx0S1bZ7y9NHmDiD6wPhgfyRb4oXwDRO4twW2NfwNr1A/exec',
     liveDashboard: '',
     trainerWorkspace: './trainer/trainer.html'
   },
