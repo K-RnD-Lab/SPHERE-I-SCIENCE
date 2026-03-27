f22fe5072a0999be287f1e3917e59c87c76c5fab
Oksana.Kolisnyk.24y.0 <zazera777@gmail.com>
2026-03-27 22:12:38 +0200

    Update config.js

--- S7 — 📚 K Life OS/S7-I · 🔎 Career or Education/R1 - Master Prep Analytics/dashboard/config.js
+++ S7 — 📚 K Life OS/S7-I · 🔎 Career or Education/R1 - Master Prep Analytics/dashboard/config.js
@@ -2,9 +2,9 @@
   dataEndpoint: '',
   links: {
     repository: 'https://github.com/K-RnD-Lab/SPHERE-I-SCIENCE/tree/main/S7%20%E2%80%94%20%F0%9F%93%9A%20Learning,%20Cognition%20%26%20Adaptive%20Systems/S7-R1%20-%20Master%20Prep%20Analytics',
-    googleSheet: 'https://docs.google.com/spreadsheets/d/REPLACE_ME/edit',
-    lookerStudio: 'https://lookerstudio.google.com/reporting/REPLACE_ME',
-    appsScript: 'https://script.google.com/macros/s/REPLACE_ME/exec',
+    googleSheet: 'https://docs.google.com/spreadsheets/d/1GcgjCJEPDAFtqOwONsfN_np5zdZFe3v2qa2lNzqDZd4/edit?gid=1089917585#gid=1089917585',
+    lookerStudio: '',
+    appsScript: 'https://script.google.com/macros/s/AKfycbzvd3zQ62mTJ6yLLCxhOG3rYRN2p8-KKCUCpbNbABOtm4BOZk1NX--L381uXmu71u2vug/exec',
     liveDashboard: 'https://REPLACE_ME.vercel.app'
   }
 };
